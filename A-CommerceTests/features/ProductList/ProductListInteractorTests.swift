554c47a893201452acf90598913971570bd587f0
Nattapon Pondongnok <nainatjab999@gmail.com>
2021-06-09 02:23:49 +0700

    add test

---
+++ A-CommerceTests/features/ProductList/ProductListInteractorTests.swift
@@ -0,0 +1,26 @@
+//
+//  ProductListInteractorTests.swift
+//  A-CommerceTests
+//
+//  Created by Nattapon Pondongnok on 9/6/2564 BE.
+//
+
+import XCTest
+@testable import A_Commerce
+
+final class ProductListInteractorTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        
+            }
+    
+    override func tearDown() {
+        // TODO: Teardown
+        super.tearDown()
+    }
+    
+    func test_givenInteractor_whenTryToLogInCalled_thenWorkerCalled() {
+        XCTAssertEqual(1, 1)
+    }
+    
+}
